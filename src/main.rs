8ad28da3e32df30475d02cc1251faf0aae8dabec
凤之兮原 <1577310596@qq.com>
2022-10-07 21:58:22 +0800

    extract: parse_config to a Stuct Config to Result

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
 use std::fs;
+use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1)
+    });
 
     println!("query : {}, filename : {}", config.query, config.filename);
 
@@ -20,12 +24,15 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Config {
+        Ok(Config {
             query,
             filename,
-        }
+        })
     }
 }
