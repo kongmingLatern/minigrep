359c0b05f1153509084d83d864c5de5c8c76d392
凤之兮原 <1577310596@qq.com>
2022-10-07 21:30:44 +0800

    feat: finsih basic read file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::fs;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("query : {}, filename : {}", query, filename);
+
+    let contents: String = fs
+        ::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
