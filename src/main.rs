3003a71dfff793ca64bfe30e10db2d0c1b3af4a2
凤之兮原 <1577310596@qq.com>
2022-10-08 22:55:53 +0800

    chore: change println! to eprintln!

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1)
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application e: {}", e);
+        eprintln!("Application e: {}", e);
         process::exit(1);
     }
 }
