23f0f80f712798edc65991e364e709b87a45da34
凤之兮原 <1577310596@qq.com>
2022-10-09 20:06:48 +0800

    feat: use filter and Iterable

--- src/lib.rs
+++ src/lib.rs
@@ -25,12 +25,27 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => {
+                return Err("Didn't get a query string");
+            }
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => {
+                return Err("Didn't get a file name");
+            }
+        };
+
         let case_insensitive = env::var("CASE_INSENSITIVE").is_err();
         Ok(Config {
             query,
@@ -41,15 +56,10 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
